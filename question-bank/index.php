c26d97f1a2641cce7a16a7356a1834d124cb7260
Aditya Sharma <aditya.sharma.met19@itbhu.ac.in>
2020-08-31 09:16:55 +0530

    asf

--- question-bank/index.php
+++ question-bank/index.php
@@ -8,12 +8,18 @@
 			echo ("HELLO YR KYA KARU MAAIN?");
 			include('includes\dbconfig.php');
 			$ref="kohbee-library-1d47d/number systems and its operations/0";
-			$fetchdata = $database->getReference($ref)->getValue();
-			foreach($fetchdata as $key => $row){
-				echo $row['id'];
-				echo ("Hogya");
+			// $fetchdata = $database->getReference($ref)->getValue();
+			$reference = $database->getReference("kohbee-library-1d47d/number systems and its operations");
+			$snapshot = $reference->getSnapshot();
+			$value = $snapshot->getValue();
+			echo ($value.exists());
+
+
+			// foreach($fetchdata as $key => $row){
+			// 	echo $row['id'];
+			// 	echo ("Hogya");
 				
-			}
+			// }
 		?>
 
 <h1>HELLO </h1>
