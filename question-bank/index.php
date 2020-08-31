256926e7cf79c6e73a23e63a3b1cff9c90767bd6
Aditya Sharma <aditya.sharma.met19@itbhu.ac.in>
2020-08-31 08:59:52 +0530

    af

--- question-bank/index.php
+++ question-bank/index.php
@@ -9,9 +9,11 @@
 			$ref="kohbee-library-1d47d/number systems and its operations";
 			$fetchdata = $database->getReference($ref)->getValue();
 			foreach($fetchdata as $key => $row){
-				echo $row['id'];
+				// echo $row['id'];
+				echo "HELLO YR KYA KARU MAAIN?";
 			}
 		?>
+
 		
 <h1>HELLO </h1>
 </body>
