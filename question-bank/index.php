4f254a7f73b1e30215c90ee0eb0e9f9400377bea
Aditya Sharma <aditya.sharma.met19@itbhu.ac.in>
2020-08-31 09:08:06 +0530

    ghf

--- question-bank/index.php
+++ question-bank/index.php
@@ -6,16 +6,16 @@
 <body>
 		<?php
 			echo ("HELLO YR KYA KARU MAAIN?");
-			include('includes/dbconfig.php');
-			$ref="kohbee-library-1d47d/number systems and its operations";
+			include('includes\dbconfig.php');
+			$ref="kohbee-library-1d47d/number systems and its operations/0";
 			$fetchdata = $database->getReference($ref)->getValue();
 			foreach($fetchdata as $key => $row){
-				// echo $row['id'];
+				echo $row['id'];
+				echo ("Hogya");
 				
 			}
 		?>
 
-		
 <h1>HELLO </h1>
 </body>
 </html>
