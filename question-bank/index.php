8816394570addf73abea6e8f41cdb3975446d4d9
Aditya Sharma <aditya.sharma.met19@itbhu.ac.in>
2020-09-01 18:55:10 +0530

    done

--- question-bank/index.php
+++ question-bank/index.php
@@ -10,27 +10,20 @@
 			error_reporting(E_ALL);
 			echo ("HELLO YR KYA KARU MAAIN?");
 			include('./includes/dbconfig.php');
-			// $ref="question-bank/number systems and its operations";
-			// $fetchdata = $database->getReference($ref)->getValue();
+			$ref="question-bank/number systems and its operations";
+			$fetchdata = $database->getReference($ref)->getValue();
 			// $reference = $database->getReference("question-bank/number systems and its operations/0");
 			// $snapshot = $reference->getSnapshot();
 			// $value = $snapshot->getValue();
 			// echo ($value.exists());
 
-			$data= [
-				'name'=> "ADITYA"
-        		];
-        	$ref="test/";
-        	$postdata=$database->getReference($ref)->push($data);
-
-
-			// foreach($fetchdata as $key => $row){
-			// 	echo $row['id'];
-			// }
+			foreach($fetchdata as $key => $row){
+				echo $row['id'];
+			}
 			echo("HOGYA");
 
 		?>
 
-<h1>HELLO 2</h1>
+<h1>HELLO 3</h1>
 </body>
 </html>
