a9a3179d36cac6996a081a1a316e42b8bd9976e8
Aditya Sharma <aditya.sharma.met19@itbhu.ac.in>
2020-09-04 07:57:16 +0530

    fuck

--- question-bank/question.php
+++ question-bank/question.php
@@ -28,69 +28,7 @@ $_SESSION["current"];
 }/**/
 
 <style>
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
 
-.navbar {
-  overflow: hidden;
-  background-color: #333;
-}
-
-.navbar a {
-  float: left;
-  font-size: 16px;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-.dropdown {
-  float: left;
-  overflow: hidden;
-}
-
-.dropdown .dropbtn {
-  font-size: 16px;  
-  border: none;
-  outline: none;
-  color: white;
-  padding: 14px 16px;
-  background-color: inherit;
-  font-family: inherit;
-  margin: 0;
-}
-
-.navbar a:hover, .dropdown:hover .dropbtn {
-  background-color: red;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
-
-.dropdown-content a {
-  float: none;
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-  text-align: left;
-}
-
-.dropdown-content a:hover {
-  background-color: #ddd;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
 html,
 body {
   margin: 0;
@@ -549,7 +487,7 @@ h2 {
     $fetchdata = $database->getReference($ref)->getValue();
     ?>
 
-
+<div>
     <?php
     if(isset($_POST['show_ques'])) {
     $_SESSION["current"]=1;
@@ -570,7 +508,7 @@ h2 {
     <?php 
     echo "<h1>";
     echo $_SESSION["current"];
-    echo "</h1>"
+    echo "</h1>";
     ?>
     <form method="post">
         <input type="submit" name="show_ques"
@@ -579,7 +517,7 @@ h2 {
                 value="Next"/>
     </form> 
     
-
+</div>
 
 
 
