edadd8bcaec736fb935c6e0a3d4c8089b694394d
Aditya Sharma <aditya.sharma.met19@itbhu.ac.in>
2020-09-06 12:49:10 +0530

    abcd

--- question-bank/action.php
+++ question-bank/action.php
@@ -2,6 +2,6 @@
 $ref="ncert-solutions/".$_POST["class"]."/".$_POST["subject"]."/".$_POST["chapter"];
 include('./includes/dbconfig.php');
 $fetchdata = $database->getReference($ref)->getValue();
-$ref=$ref.$fetchdata[0];
+$ref=$ref.$fetchdata[1];
 echo $ref;
 ?>
