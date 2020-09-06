88ef95175ca68c70b86c38e2d5ad9345f6a07f03
Aditya Sharma <aditya.sharma.met19@itbhu.ac.in>
2020-09-06 14:23:36 +0530

    Update string-expression.php

--- question-bank/string-expression.php
+++ question-bank/string-expression.php
@@ -35,20 +35,20 @@
 
   <label for="chapter">Chapter:</label>
   <select name="chapter" id="chapter">
-    <option value="chapter1/Knowing our Numbers">Chapter 1</option>
-    <option value="chapter2/Whole Numbers">Chapter 2</option>
-    <option value="chapter3/Playing with Numbers">Chapter 3</option>
-    <option value="chapter4/Basic Geometrical Ideas">Chapter 4</option>
-    <option value="chapter5/Understanding Elementary Shapes">Chapter 5</option>
-    <option value="chapter6/Integers">Chapter 6</option>
-    <option value="chapter7/Fractions">Chapter 7</option>
-    <option value="chapter8/Decimals">Chapter 8</option>
-    <option value="chapter9/Data Handling">Chapter 9</option>
-    <option value="chapter10/Mensuration">Chapter 10</option>
-    <option value="chapter11/Algebra">Chapter 11</option>
-    <option value="chapter12/Ratio and Proportion">Chapter 12</option>
-    <option value="chapter13/Symmetry">Chapter 13</option>
-    <option value="chapter14/Practical Geometry">Chapter 14</option>
+    <option value="chapter1/ Knowing our Numbers">Chapter 1</option>
+    <option value="chapter2/ Whole Numbers">Chapter 2</option>
+    <option value="chapter3/ Playing with Numbers">Chapter 3</option>
+    <option value="chapter4/ Basic Geometrical Ideas">Chapter 4</option>
+    <option value="chapter5/ Understanding Elementary Shapes">Chapter 5</option>
+    <option value="chapter6/ Integers">Chapter 6</option>
+    <option value="chapter7/ Fractions">Chapter 7</option>
+    <option value="chapter8/ Decimals">Chapter 8</option>
+    <option value="chapter9/ Data Handling">Chapter 9</option>
+    <option value="chapter10/ Mensuration">Chapter 10</option>
+    <option value="chapter11/ Algebra">Chapter 11</option>
+    <option value="chapter12/ Ratio and Proportion">Chapter 12</option>
+    <option value="chapter13/ Symmetry">Chapter 13</option>
+    <option value="chapter14/ Practical Geometry">Chapter 14</option>
   </select>
   <br><br>
   <input type="submit" value="Submit">
