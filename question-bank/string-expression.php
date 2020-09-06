3c6ab0fc5123262406c14f0e69816ec72d23ad9d
Aditya Sharma <aditya.sharma.met19@itbhu.ac.in>
2020-09-06 17:33:33 +0530

    Update string-expression.php

--- question-bank/string-expression.php
+++ question-bank/string-expression.php
@@ -50,7 +50,6 @@
     echo '<select name="subject" id="subject">';
     
   $ref="ncert-solutions/".$_POST['class'];
-  echo $ref;
   $fetchdata = $database->getReference($ref)->getValue();
   foreach($fetchdata as $key => $value) {  
     echo '<option value="';
@@ -65,35 +64,44 @@
   echo '<input type="submit" value="Click Here">';
   echo '</form>';
 }
-  ?>
-
-  <form method="POST">
-  <label for="chapter">Chapter:</label>
-  <select name="chapter" id="chapter">
-    <?php
-    ?>
+?>
 
 
+  <?php
+    if(isset($_POST['subject'])){
+  echo '<form method="POST">';
+  echo '<label for="chapter">Subject:</label>';
+  echo '<select name="chapter" id="chapter">';
+    $ref="ncert-solutions/".$_POST['class']."/".$_POST['subject'];
+    $fetchdata = $database->getReference($ref)->getValue();
 
-    <option value="chapter1/ Knowing our Numbers">Chapter 1</option>
-    <option value="chapter2/ Whole Numbers">Chapter 2</option>
-    <option value="chapter3/ Playing with Numbers">Chapter 3</option>
-    <option value="chapter4/ Basic Geometrical Ideas">Chapter 4</option>
-    <option value="chapter5/ Understanding Elementary Shapes">Chapter 5</option>
-    <option value="chapter6/ Integers">Chapter 6</option>
-    <option value="chapter7/ Fractions">Chapter 7</option>
-    <option value="chapter8/ Decimals">Chapter 8</option>
-    <option value="chapter9/ Data Handling">Chapter 9</option>
-    <option value="chapter10/ Mensuration">Chapter 10</option>
-    <option value="chapter11/ Algebra">Chapter 11</option>
-    <option value="chapter12/ Ratio and Proportion">Chapter 12</option>
-    <option value="chapter13/ Symmetry">Chapter 13</option>
-    <option value="chapter14/ Practical Geometry">Chapter 14</option>
-  </select>
-  <br><br>
-  <input type="submit" value="Submit">
-</form>
+    foreach($fetchdata as $key => $value) {  
+    echo '<option value="';
+    echo $key;
+    echo '">';
+    echo $key;
+    echo '</option>';
+    }
 
+    // <option value="chapter1/ Knowing our Numbers">Chapter 1</option>
+    // <option value="chapter2/ Whole Numbers">Chapter 2</option>
+    // <option value="chapter3/ Playing with Numbers">Chapter 3</option>
+    // <option value="chapter4/ Basic Geometrical Ideas">Chapter 4</option>
+    // <option value="chapter5/ Understanding Elementary Shapes">Chapter 5</option>
+    // <option value="chapter6/ Integers">Chapter 6</option>
+    // <option value="chapter7/ Fractions">Chapter 7</option>
+    // <option value="chapter8/ Decimals">Chapter 8</option>
+    // <option value="chapter9/ Data Handling">Chapter 9</option>
+    // <option value="chapter10/ Mensuration">Chapter 10</option>
+    // <option value="chapter11/ Algebra">Chapter 11</option>
+    // <option value="chapter12/ Ratio and Proportion">Chapter 12</option>
+    // <option value="chapter13/ Symmetry">Chapter 13</option>
+    // <option value="chapter14/ Practical Geometry">Chapter 14</option>
+  echo '</select>';
+  echo '<input type="submit" value="Submit">';
+  echo '</form>';
+  
+}?>
 <p>Click the "Submit" button and the form-data will be sent to a page on the 
 server called "action_page.php".</p>
 
