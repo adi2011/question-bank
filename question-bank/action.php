7bab1cb1685783c99e83a62aa2c5a6f0f0994bf9
Aditya Sharma <aditya.sharma.met19@itbhu.ac.in>
2020-09-06 12:47:23 +0530

    ma ki chut

--- question-bank/action.php
+++ question-bank/action.php
@@ -2,6 +2,6 @@
 $ref="ncert-solutions/".$_POST["class"]."/".$_POST["subject"]."/".$_POST["chapter"];
 include('./includes/dbconfig.php');
 $fetchdata = $database->getReference($ref)->getValue();
-$ref=$ref.$fetchdata;
+$ref=$ref.$fetchdata[0];
 echo $ref;
 ?>
