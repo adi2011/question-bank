3be21104a399e5c4256886ee71c3ad583d06cfa0
Aditya Sharma <aditya.sharma.met19@itbhu.ac.in>
2020-09-06 15:32:35 +0530

    Update string-expression.php

--- question-bank/string-expression.php
+++ question-bank/string-expression.php
@@ -21,7 +21,7 @@
     include('./includes/dbconfig.php');
     $ref="ncert-solutions";
     $fetchdata = $database->getReference($ref)->getValue();
-    echo $fetchdata;
+    echo $fetchdata[0];
     ?>
 
 <form action="./question.php" method="POST">
