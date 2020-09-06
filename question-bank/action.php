471bf89dcc7e84588275aeeaef55773c9fe7d489
Aditya Sharma <aditya.sharma.met19@itbhu.ac.in>
2020-09-06 12:53:01 +0530

    update

--- question-bank/action.php
+++ question-bank/action.php
@@ -1,7 +1,12 @@
 <?php
+ini_set('display_errors', 1); 
+ini_set('display_startup_errors', 1); 
+error_reporting(E_ALL);
 $ref="ncert-solutions/".$_POST["class"]."/".$_POST["subject"]."/".$_POST["chapter"];
 include('./includes/dbconfig.php');
 $fetchdata = $database->getReference($ref)->getValue();
-$ref=$ref.$fetchdata[1];
-echo $ref;
+foreach($fetchdata as $key => $row){
+echo $row['exercise'];
+}
+
 ?>
